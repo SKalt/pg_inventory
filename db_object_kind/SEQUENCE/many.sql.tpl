73a589dfe6bf293eb7e396cc7fbaa64cb329a32f
Steven Kalt <kalt.steven@gmail.com>
2022-10-14 20:25:20 -0400

    fix: incorporate info from pg_sequence

--- db_object_kind/SEQUENCE/many.sql.tpl
+++ db_object_kind/SEQUENCE/many.sql.tpl
@@ -1,1 +1,42 @@
-../TABLE/many.sql.tpl
+SELECT
+    rel.schema_name
+  , rel.schema_oid
+  , rel.name
+  , rel.oid
+  , rel.tablespace_name
+  , rel.tablespace_oid
+  , rel.file_node_oid
+  , rel.owner
+  , rel.owner_oid
+  , rel.acl
+  , rel.description
+  , rel.replica_identity
+{{- if not .filter_by_persistence }}
+  , rel.persistence
+{{- end }}
+  , rel.approximate_number_of_rows
+  , rel.partition_bound
+  , rel.n_pages
+  , rel.n_pages_all_visible
+  , rel.n_user_columns
+-- sequence-specific info
+  , seq.seqtypid AS type_oid
+    -- Data type of the sequence
+  , seq.seqstart AS start -- int8
+  , seq.seqincrement AS increment -- int8
+  , seq.seqmax AS max -- int8
+  , seq.seqmin AS min -- int8
+  , seq.seqcache AS cache_size -- int8
+  , seq.seqcycle AS does_cycle -- bool
+  , seq.seqtypid AS sequence_type_oid
+  , type_.typname AS sequence_type_name
+  , type_schema.nspname AS sequence_type_schema_name
+  , pg_catalog.pg_get_userbyid(type_.typowner) AS sequence_type_owner_name
+FROM pg_catalog.pg_sequence AS seq -- TODO
+INNER JOIN (
+  {{ include . "file://./../TABLE/many.sql.tpl" | indent 1 }}
+) AS rel ON seq.seqrelid = rel.oid
+INNER JOIN pg_catalog.pg_type AS type_
+  ON seq.seqtypid = type_.oid
+INNER JOIN pg_catalog.pg_namespace as type_schema
+  ON type_.typnamespace = type_schema.oid
--- db_object_kind/SEQUENCE/queries/sequences_filtered_by_persistence/query.sql
+++ db_object_kind/SEQUENCE/queries/sequences_filtered_by_persistence/query.sql
@@ -1,57 +1,97 @@
 SELECT
-  -- namespacing and ownership
-      ns.nspname AS schema_name
-    , cls.relnamespace AS schema_oid
-    , cls.relname AS name
-    , cls.oid
-    , cls_space.spcname AS tablespace_name
-    , cls.reltablespace AS tablespace_oid
-    , cls.relfilenode AS file_node_oid
-    , pg_catalog.pg_get_userbyid(cls.relowner) AS owner
-    , cls.relowner AS owner_oid
-    , cls.relacl AS acl -- aclitem[]
-  -- access method details -- omitted for classes other than tables and indices
-  -- details
-    , pg_catalog.obj_description(cls.oid, 'pg_class') AS "description" -- comment?
-    , cls.relreplident AS replica_identity -- char
-      -- Columns used to form "replica identity" for rows:
-      -- d = default (primary key, if any)
-      -- n = nothing
-      -- f = all columns
-      -- i = index with indisreplident set (same as nothing if the index used has been dropped)
-    , cls.relpersistence AS persistence
-      -- p => permanent table
-      -- u => unlogged table: not dropped at a session
-      -- t => temporary table: unlogged **and** dropped at the end of a session.
-    , cls.reltuples AS approximate_number_of_rows
-    , (
-        CASE
-          WHEN cls.relispartition THEN pg_catalog.pg_get_expr(cls.relpartbound, cls.oid, true)
-          ELSE NULL
+    rel.schema_name
+  , rel.schema_oid
+  , rel.name
+  , rel.oid
+  , rel.tablespace_name
+  , rel.tablespace_oid
+  , rel.file_node_oid
+  , rel.owner
+  , rel.owner_oid
+  , rel.acl
+  , rel.description
+  , rel.replica_identity
+  , rel.persistence
+  , rel.approximate_number_of_rows
+  , rel.partition_bound
+  , rel.n_pages
+  , rel.n_pages_all_visible
+  , rel.n_user_columns
+-- sequence-specific info
+  , seq.seqtypid AS type_oid
+    -- Data type of the sequence
+  , seq.seqstart AS start -- int8
+  , seq.seqincrement AS increment -- int8
+  , seq.seqmax AS max -- int8
+  , seq.seqmin AS min -- int8
+  , seq.seqcache AS cache_size -- int8
+  , seq.seqcycle AS does_cycle -- bool
+  , seq.seqtypid AS sequence_type_oid
+  , type_.typname AS sequence_type_name
+  , type_schema.nspname AS sequence_type_schema_name
+  , pg_catalog.pg_get_userbyid(type_.typowner) AS sequence_type_owner_name
+FROM pg_catalog.pg_sequence AS seq -- TODO
+INNER JOIN (
+  SELECT
+    -- namespacing and ownership
+        ns.nspname AS schema_name
+      , cls.relnamespace AS schema_oid
+      , cls.relname AS name
+      , cls.oid
+      , cls_space.spcname AS tablespace_name
+      , cls.reltablespace AS tablespace_oid
+      , cls.relfilenode AS file_node_oid
+      , pg_catalog.pg_get_userbyid(cls.relowner) AS owner
+      , cls.relowner AS owner_oid
+      , cls.relacl AS acl -- aclitem[]
+    -- access method details -- omitted for classes other than tables and indices
+    -- details
+      , pg_catalog.obj_description(cls.oid, 'pg_class') AS "description" -- comment?
+      , cls.relreplident AS replica_identity -- char
+        -- Columns used to form "replica identity" for rows:
+        -- d = default (primary key, if any)
+        -- n = nothing
+        -- f = all columns
+        -- i = index with indisreplident set (same as nothing if the index used has been dropped)
+      , cls.relpersistence AS persistence
+        -- p => permanent table
+        -- u => unlogged table: not dropped at a session
+        -- t => temporary table: unlogged **and** dropped at the end of a session.
+      , cls.reltuples AS approximate_number_of_rows
+      , (
+          CASE
+            WHEN cls.relispartition THEN pg_catalog.pg_get_expr(cls.relpartbound, cls.oid, true)
+            ELSE NULL
+          END
+        ) AS partition_bound
+      , cls.relpages AS n_pages -- int4: updated by vacuum, analyze, create index
+      , cls.relallvisible AS n_pages_all_visible
+      , cls.relnatts AS n_user_columns
+        -- Number of user columns in the relation (system columns not counted).
+        -- There must be this many corresponding entries in pg_attribute.
+      , cls.relchecks AS n_check_constraints
+        -- int2; see pg_constraint catalog
+  FROM pg_catalog.pg_class AS cls -- https://www.postgresql.org/docs/current/catalog-pg-class.html
+  INNER JOIN pg_catalog.pg_namespace AS ns -- see https://www.postgresql.org/docs/current/catalog-pg-namespace.html
+    ON
+      cls.relkind = 'S' AND
+      ( --validate input parameter: persistence
+        CASE :'persistence'
+          WHEN 'p' THEN true -- permanent table
+          WHEN 'u' THEN true -- unlogged table: not dropped at a session
+          WHEN 't' THEN true -- temporary table: unlogged **and** dropped at the end of a session.
+          ELSE (1/0)::BOOLEAN -- error: parameter persistence must be either 'p', 'u', or 't'
         END
-      ) AS partition_bound
-    , cls.relpages AS n_pages -- int4: updated by vacuum, analyze, create index
-    , cls.relallvisible AS n_pages_all_visible
-    , cls.relnatts AS n_user_columns
-      -- Number of user columns in the relation (system columns not counted).
-      -- There must be this many corresponding entries in pg_attribute.
-    , cls.relchecks AS n_check_constraints
-      -- int2; see pg_constraint catalog
-FROM pg_catalog.pg_class AS cls -- https://www.postgresql.org/docs/current/catalog-pg-class.html
-INNER JOIN pg_catalog.pg_namespace AS ns -- see https://www.postgresql.org/docs/current/catalog-pg-namespace.html
-  ON
-    cls.relkind = 'S' AND
-    ( --validate input parameter: persistence
-      CASE :'persistence'
-        WHEN 'p' THEN true -- permanent table
-        WHEN 'u' THEN true -- unlogged table: not dropped at a session
-        WHEN 't' THEN true -- temporary table: unlogged **and** dropped at the end of a session.
-        ELSE (1/0)::BOOLEAN -- error: parameter persistence must be either 'p', 'u', or 't'
-      END
-    ) AND
-    cls.relpersistence = :'persistence' AND
-    cls.relnamespace = ns.oid
-LEFT JOIN pg_catalog.pg_am AS access_method -- https://www.postgresql.org/docs/current/catalog-pg-am.html
-  ON cls.relam > 0 AND cls.relam = access_method.oid
-LEFT JOIN pg_catalog.pg_tablespace AS cls_space -- see https://www.postgresql.org/docs/current/catalog-pg-tablespace.html
-  ON (cls.reltablespace = cls_space.oid)
+      ) AND
+      cls.relpersistence = :'persistence' AND
+      cls.relnamespace = ns.oid
+  LEFT JOIN pg_catalog.pg_am AS access_method -- https://www.postgresql.org/docs/current/catalog-pg-am.html
+    ON cls.relam > 0 AND cls.relam = access_method.oid
+  LEFT JOIN pg_catalog.pg_tablespace AS cls_space -- see https://www.postgresql.org/docs/current/catalog-pg-tablespace.html
+    ON (cls.reltablespace = cls_space.oid)
+  
+) AS rel ON seq.seqrelid = rel.oid
+INNER JOIN pg_catalog.pg_type AS type_
+  ON seq.seqtypid = type_.oid
+INNER JOIN pg_catalog.pg_namespace as type_schema
+  ON type_.typnamespace = type_schema.oid
