71a932cc3a2aabef765e4b8a9832735ed2bc1f69
Steven Kalt <SKalt@users.noreply.github.com>
2022-11-26 18:21:04 -0500

    docs: link pg_tablespace reference docs

--- db_object_kind/TABLESPACE/queries/all/query.sql
+++ db_object_kind/TABLESPACE/queries/all/query.sql
@@ -4,4 +4,4 @@ SELECT
   , pg_catalog.pg_tablespace_location(oid) AS "location"
   , tablespace.spcacl AS access_privileges
   , tablespace.spcoptions AS options -- keyword=value strings
-FROM pg_catalog.pg_tablespace AS tablespace
+FROM pg_catalog.pg_tablespace AS tablespace -- see https://www.postgresql.org/docs/current/catalog-pg-tablespace.html
