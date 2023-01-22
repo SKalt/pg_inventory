fc5c1ec8b1b471b2a567954ff44fb45a4c7d86f8
Steven Kalt <kalt.steven@gmail.com>
2023-01-22 09:05:08 -0500

    feat(fdw)!: add oid option
    BREAKING CHANGE: rename fdw query

---
+++ db_object_kind/FOREIGN_DATA_WRAPPER/queries/all_foreign_data_wrappers_normalized/query.sql
@@ -0,0 +1,10 @@
+SELECT
+  -- namespacing
+    fdw.oid
+  ,fdw.fdwname AS "name"
+  , fdw.fdwowner AS owner_oid
+  , fdw.fdwacl AS access_privileges
+  , fdw.fdwoptions AS options
+  , fdw.fdwhandler AS handler_fn_oid
+  , fdw.fdwvalidator AS validator_fn_oid
+FROM pg_catalog.pg_foreign_data_wrapper AS fdw -- https://www.postgresql.org/docs/current/catalog-pg-foreign-data-wrapper.html
