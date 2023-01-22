73bf2a3997488362dec331b2058b1a7f35310782
Steven Kalt <kalt.steven@gmail.com>
2023-01-22 16:45:39 -0500

    feat(tablespace)!: fetch comments
    BREAKING CHANGE: add trailing "comment" column

--- db_object_kind/TABLESPACE/queries/all/query.sql
+++ db_object_kind/TABLESPACE/queries/all/query.sql
@@ -4,4 +4,5 @@ SELECT
   , pg_catalog.pg_tablespace_location(oid) AS "location"
   , tablespace.spcacl AS access_privileges
   , tablespace.spcoptions AS options -- keyword=value strings
+  , pg_catalog.shobj_description(tablespace.oid, 'pg_table') AS "comment"
 FROM pg_catalog.pg_tablespace AS tablespace -- see https://www.postgresql.org/docs/current/catalog-pg-tablespace.html
